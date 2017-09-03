84624b3e866012451b4c003bec133d610c16ca40
marctuscher <tuscher.marc@googlemail.com>
2017-09-03 22:13:19 +0200

    changed theme

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
 ;; === List my packages ===
 ;; simply add package names to the list
 (ensure-package-installed
- 'dracula-theme
+ 'color-theme-sanityinc-tomorrow
  'better-defaults
  'auctex
  'yasnippet
@@ -40,8 +40,6 @@
  'js2-mode
  'ac-js2
  'auto-complete
- 'tern
- 'tern-auto-complete
  'nyan-mode
  'angular-mode
  'web-mode
@@ -51,7 +49,6 @@
  'multiple-cursors
 )
 
-(load-theme 'dracula t)
 
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
@@ -84,17 +81,6 @@
 (global-set-key [f8] 'neotree-toggle)
 
 
-;;(minimap-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(minimap-minimum-width 0)
- '(minimap-window-location (quote right))
- '(package-selected-packages
-   (quote
-    (multiple-cursors web-mode volatile-highlights use-package tern-auto-complete paredit nyan-mode neotree minimap helm-projectile flycheck elpy dracula-theme dashboard company-auctex better-defaults anzu angular-mode ac-js2))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -159,10 +145,11 @@
 
 ;;;;javascript and webdev extensions
 ;; open json files with js-mode
-(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
-(add-hook 'js-mode-hook 'js2-minor-mode)
-(add-hook 'js2-mode-hook 'ac-js2-mode)
-(setq js2-highlight-level 3)
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; Better imenu
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 ;;; auto complete mode
 ;;; should be loaded after yasnippet so that they can work together
 (require 'auto-complete-config)
@@ -174,11 +161,69 @@
 (ac-set-trigger-key "TAB")
 (ac-set-trigger-key "<tab>")
 ;;; npm install -g tern
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
-(eval-after-load 'tern
-   '(progn
-      (require 'tern-auto-complete)
-      (tern-ac-setup)))
 
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#ffffff" "#ff9da4" "#d1f1a9" "#ffeead" "#bbdaff" "#ebbbff" "#99ffff" "#003f8e"))
+ '(compilation-message-face (quote default))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
+ '(custom-safe-themes
+   (quote
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+ '(fci-rule-color "#003f8e")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors
+   (quote
+    (("#3C3D37" . 0)
+     ("#679A01" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#3C3D37" . 100))))
+ '(magit-diff-use-overlays nil)
+ '(minimap-minimum-width 0)
+ '(minimap-window-location (quote right))
+ '(package-selected-packages
+   (quote
+    (multiple-cursors web-mode volatile-highlights use-package tern-auto-complete paredit nyan-mode neotree minimap helm-projectile flycheck elpy dracula-theme dashboard company-auctex better-defaults anzu angular-mode ac-js2)))
+ '(pos-tip-background-color "#FFFACE")
+ '(pos-tip-foreground-color "#272822")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#ff9da4")
+     (40 . "#ffc58f")
+     (60 . "#ffeead")
+     (80 . "#d1f1a9")
+     (100 . "#99ffff")
+     (120 . "#bbdaff")
+     (140 . "#ebbbff")
+     (160 . "#ff9da4")
+     (180 . "#ffc58f")
+     (200 . "#ffeead")
+     (220 . "#d1f1a9")
+     (240 . "#99ffff")
+     (260 . "#bbdaff")
+     (280 . "#ebbbff")
+     (300 . "#ff9da4")
+     (320 . "#ffc58f")
+     (340 . "#ffeead")
+     (360 . "#d1f1a9"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
