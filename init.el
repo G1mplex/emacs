475b770937c3cce8fed904484c90577f677a2197
marctuscher <tuscher.marc@googlemail.com>
2017-09-03 22:29:13 +0200

    changed theme

--- init.el
+++ init.el
@@ -47,6 +47,7 @@
  'elpy
  'anzu
  'multiple-cursors
+ 'tide
 )
 
 
@@ -146,7 +147,7 @@
 ;;;;javascript and webdev extensions
 ;; open json files with js-mode
 (require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-modeg))
 
 ;; Better imenu
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
@@ -168,6 +169,33 @@
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+
+
+(setq font-lock-maximum-decoration t)
+
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
